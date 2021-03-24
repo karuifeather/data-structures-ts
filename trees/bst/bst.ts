8d8ef875d6fb9749bb785606582f91ce04a7b79c
karuifeather <aryalaashaya@gmail.com>
2021-03-24 12:05:21 +0545

    implemented removal of node with 0 or 1 child

--- trees/bst/bst.ts
+++ trees/bst/bst.ts
@@ -53,13 +53,111 @@ export class BST {
 
     return found;
   }
+
+  find(value: number): BSTNode {
+    if (!this.root) return undefined;
+
+    let current: BSTNode | null = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) current = current.left;
+      else if (value > current.value) current = current.right;
+      else found = true;
+    }
+
+    return current;
+  }
+
+  private dealWith(node: BSTNode, parent: BSTNode, children: number): void {
+    if (children === 0) {
+      if (node.value > parent.value) parent.right = null;
+      else if (node.value < parent.value) parent.left = null;
+    }
+
+    if (children === 1) {
+      let childIsOnTheLeft: boolean;
+
+      if (node.left) childIsOnTheLeft = true;
+
+      if (node.value > parent.value) {
+        if (childIsOnTheLeft) parent.right = node.left;
+        else parent.right = node.right;
+      } else if (node.value < parent.value) {
+        if (childIsOnTheLeft) parent.left = node.left;
+        else parent.left = node.right;
+      }
+    }
+
+    if (children === 2) {
+      // Do something
+    }
+
+    return;
+  }
+
+  remove(value: number): BSTNode {
+    if (!this.root) return undefined;
+
+    let targetNode: BSTNode | null = this.root;
+    let parent: BSTNode;
+    let found = false;
+
+    while (targetNode && !found) {
+      if (value < targetNode.value) {
+        parent = targetNode;
+        targetNode = targetNode.left;
+      } else if (value > targetNode.value) {
+        parent = targetNode;
+        targetNode = targetNode.right;
+      } else {
+        found = true;
+      }
+    }
+
+    if (!found) return;
+
+    if (targetNode.left && targetNode.right) {
+      this.dealWith(targetNode, parent, 2);
+    } else if (targetNode.left || targetNode.right) {
+      this.dealWith(targetNode, parent, 1);
+    } else this.dealWith(targetNode, parent, 0);
+
+    targetNode.left = targetNode.right = null;
+    return targetNode;
+  }
+
+  inOrder(): BSTNode[] {
+    const nodes: BSTNode[] = [];
+    const node = this.root;
+
+    const traverse = (node: BSTNode): void => {
+      nodes.push(node);
+      node.left && traverse(node.left);
+      node.right && traverse(node.right);
+    };
+
+    traverse(node);
+
+    return nodes;
+  }
 }
 
-// const tree = new BST();
-// tree.insert(234);
-// tree.insert(2);
-// tree.insert(345);
+const tree = new BST();
+tree.insert(60);
+tree.insert(28);
+tree.insert(82);
+tree.insert(12);
+tree.insert(46);
+tree.insert(75);
+tree.insert(95);
+tree.insert(15);
+tree.insert(58);
+tree.insert(87);
 // tree.insert(403);
 // tree.insert(1023);
 // tree.insert(786);
-// console.log(tree.contains(403));
+console.log(tree.inOrder());
+
+console.log(tree.remove(12));
+console.log(tree.inOrder());
